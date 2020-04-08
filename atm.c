65cede5bbd278b23e84812c82437c6eedd2155da
sukhiboi <40167376+sukhiboi@users.noreply.github.com>
2020-04-08 13:22:21 +0530

    Updated code with bitwise operators

--- atm.c
+++ atm.c
@@ -10,7 +10,8 @@ unsigned int get_money(unsigned short int amount)
     int demonitions[8] = {2000, 500, 100, 50, 20, 10, 5, 1};
     for (int i = 0; i < 8; i++)
     {
-        notesCount += pow(16, 7-i) * (total / demonitions[i]);
+        notesCount <<= 4;
+        notesCount |= total / demonitions[i];
         total %= demonitions[i];
     }
     return notesCount;
